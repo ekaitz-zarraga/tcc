2b6271d1d557f62a38740a447a3d14a17e381068
Jan Nieuwenhuizen <janneke@gnu.org>
2018-05-22 01:08:03 +0200

    bootstrappable: Work around MesCC bug.
    * tccelf.c (squeeze_multi_relocs): Simplify expressions.

--- tccelf.c
+++ tccelf.c
@@ -568,9 +568,13 @@ ST_FUNC void squeeze_multi_relocs(Section *s, size_t oldrelocoffset)
        a simple insertion sort.  */
     for (a = oldrelocoffset + sizeof(*r); a < sr->data_offset; a += sizeof(*r)) {
 	ssize_t i = a - sizeof(*r);
-	addr = ((ElfW_Rel*)(sr->data + a))->r_offset;
+	///addr = ((ElfW_Rel*)(sr->data + a))->r_offset;
+	ElfW_Rel* ea = (ElfW_Rel*)(sr->data + a);
+	ElfW_Rel* ei = (ElfW_Rel*)(sr->data + i);
+	addr = ea->r_offset;
 	for (; i >= (ssize_t)oldrelocoffset &&
-	       ((ElfW_Rel*)(sr->data + i))->r_offset > addr; i -= sizeof(*r)) {
+               ei->r_offset > addr; i -= sizeof(*r)) {
+            ei = (ElfW_Rel*)(sr->data + i);
 	    ElfW_Rel tmp = *(ElfW_Rel*)(sr->data + a);
 	    *(ElfW_Rel*)(sr->data + a) = *(ElfW_Rel*)(sr->data + i);
 	    *(ElfW_Rel*)(sr->data + i) = tmp;
