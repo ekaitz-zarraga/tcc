ef55a31795a566ea03b4f8df34b6fa0a3f969145
Ekaitz Zarraga <ekaitz@elenq.tech>
2022-08-08 18:30:21 +0200

    Add a manifest file for running the compiler

---
+++ manifest.scm
@@ -0,0 +1,12 @@
+(load "guix.scm")
+
+(define triplet "riscv64-linux-gnu")
+
+(packages->manifest
+  (let* ((binutils (cross-binutils triplet))
+         (libc     (cross-libc     triplet)))
+    (list tcc-mine
+          (list gcc "lib")
+          binutils
+          libc
+          (list libc "static"))))
