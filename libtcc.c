f908804fbe9781639c7cbe577df7c08bf9e63495
Ekaitz Zarraga <ekaitz@elenq.tech>
2024-03-14 20:30:50 +0100

    bootstrap: Force static link.

--- libtcc.c
+++ libtcc.c
@@ -801,6 +801,7 @@ LIBTCCAPI TCCState *tcc_new(void)
     s->gnu_ext = 1;
     s->tcc_ext = 1;
     s->nocommon = 1;
+    s->static_link = 1;
     s->dollars_in_identifiers = 1; /*on by default like in gcc/clang*/
     s->cversion = 199901; /* default unless -std=c11 is supplied */
     s->warn_implicit_function_declaration = 1;
